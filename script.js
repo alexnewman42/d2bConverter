39cbe23ee0685f73ecb119e442ce7eae13b71015
root <alexnewman42@gmail.com>
2024-05-22 07:56:16 -0400

    Finish decimalToBinary function

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ const decimalToBinary = (input) => {
     const quotients = [];
     const remainders = [];
 
+    if (input === 0) {
+        result.innerText = "0";
+        return;
+    }
+
     while (input > 0) {
         const quotient = Math.floor(input / 2);
+        const remainder = input % 2;
+        inputs.push(input);
+        quotients.push(quotient);
+        remainders.push(remainder);
         input = quotient;
     }
+    result.innerText = remainders.reverse().join("");
 };
 
 const checkUserInput = () => {
