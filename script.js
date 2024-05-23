6cbc37d0c50a53715938f5d0dd1867e080e68393
root <alexnewman42@gmail.com>
2024-05-23 09:51:42 -0400

    Remove contents of decimalToBinary function

--- script.js
+++ script.js
@@ -3,24 +3,8 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-    const inputs = [];
-    const quotients = [];
-    const remainders = [];
+ 
 
-    if (input === 0) {
-        result.innerText = "0";
-        return;
-    }
-
-    while (input > 0) {
-        const quotient = Math.floor(input / 2);
-        const remainder = input % 2;
-        inputs.push(input);
-        quotients.push(quotient);
-        remainders.push(remainder);
-        input = quotient;
-    }
-    result.innerText = remainders.reverse().join("");
 };
  
 const checkUserInput = () => {
