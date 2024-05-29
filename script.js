a485830d3b46d9320f2f97147dd4d0e7685374ea
root <alexnewman42@gmail.com>
2024-05-29 11:02:55 -0400

    Create base case

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const countdown = (number) => {};
+const countdown = (number) => {
+    if (number === 0) return;
+};
 
 const decimalToBinary = (input) => {
     let binary = "";
